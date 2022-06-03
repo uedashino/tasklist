e2bce1483d7a4d545379882f9494c9890eadb8ea
shino.ueda <shino_ueda@r.recruit.co.jp>
2022-06-03 19:19:47 +0900

    修正①：message⇒task

--- src/controllers/UpdateServlet.java
+++ src/controllers/UpdateServlet.java
@@ -57,7 +57,7 @@ public class UpdateServlet extends HttpServlet {
 
                 // フォームに初期値を設定、さらにエラーメッセージを送る
                 request.setAttribute("_token", request.getSession().getId());
-                request.setAttribute("message", m);
+                request.setAttribute("task", m);
                 request.setAttribute("errors", errors);
 
                 RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/edit.jsp");
@@ -70,7 +70,7 @@ public class UpdateServlet extends HttpServlet {
                 em.close();
 
                 // セッションスコープ上の不要になったデータを削除
-                request.getSession().removeAttribute("message_id");
+                request.getSession().removeAttribute("task_id");
 
                 // indexページへリダイレクト
                 response.sendRedirect(request.getContextPath() + "/index");
